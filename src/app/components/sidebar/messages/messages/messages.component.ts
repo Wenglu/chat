db85242b93f6e418abaec8c855e5bd06ca4f5faa
Wenglu <112478429+Wenglu@users.noreply.github.com>
2024-10-13 20:19:20 +0200

    feat: added navbar to messages container

--- src/app/components/sidebar/messages/messages/messages.component.ts
+++ src/app/components/sidebar/messages/messages/messages.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { UserMessageComponent } from '../../../user-message/user-message.component';
+import { NavbarComponent } from '../../../navbar/navbar.component';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [UserMessageComponent,NavbarComponent],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss'
 })
