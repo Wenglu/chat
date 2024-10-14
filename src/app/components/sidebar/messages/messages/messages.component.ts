99f87c15834b2818e89a4b0fef70418ce68e8362
Wenglu <112478429+Wenglu@users.noreply.github.com>
2024-10-14 19:02:50 +0200

    feat(added navbar component , added messages component to messages container )

--- src/app/components/sidebar/messages/messages/messages.component.ts
+++ src/app/components/sidebar/messages/messages/messages.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { UserMessageComponent } from '../../../user-message/user-message.component';
 import { NavbarComponent } from '../../../navbar/navbar.component';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [UserMessageComponent,NavbarComponent],
+  imports: [UserMessageComponent,NavbarComponent,MatIcon,MatInputModule,MatFormFieldModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss'
 })
