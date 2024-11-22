e6f07f0ca7e4e24b4b08a3db78c49b096ad0db4f
Wenglu <112478429+Wenglu@users.noreply.github.com>
2024-11-22 18:06:47 +0100

    feat: added user photo to sidebar component

--- src/app/components/sidebar/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar/sidebar.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,OnInit  } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthContextService } from '../../../services/auth-context.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatListModule,MatSidenavModule,MatIconModule,RouterLink],
+  imports: [MatListModule,MatSidenavModule,MatIconModule,RouterLink,CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  userCredential:any
 
+  constructor(private authContextService: AuthContextService){}
+
+  ngOnInit() {
+    // Subscribe to userCredential$ to get the latest user credential data
+     this.authContextService.userCredential$.subscribe((credential) => {
+      this.userCredential = credential;
+      console.log (credential)
+    });
+  }
 }
